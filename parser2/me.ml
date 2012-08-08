e4c8fbb4995dab7ece9d90f1a728dc3679904758
john <john@john-virtual-machine.(none)>
2012-08-09 00:34:35 +0800

    compiles

--- parser2/me.ml
+++ parser2/me.ml
@@ -73,6 +73,8 @@ let lead = fun e -> let e_node = e.md.a.(e.m).n in
   if e_node.pMHI < 0 then e_node.pMHI <- append {n=new_melement '<'; d=[|e.m|]} e.md;
   {e with m=e_node.pMHI}
 
+let (~<) = lead 
+
 let trail = fun e -> let e_node = e.md.a.(e.m).n in
   if e_node.pIHM < 0 then e_node.pIHM <- append {n=new_melement '>'; d=[|e.m|]} e.md;
   {e with m=e_node.pIHM}
@@ -83,6 +85,7 @@ let (+:) = fun e g -> if ( e.md == g.md && e.fd == g.fd )
 
 type cacheU = { pre_false: int ; pre_true: int; all_p: bool; some_q: bool }  
 
+(*
 let build_cacheU d fd f = let ca = Array.make d.len {pre_false=false; pre_true=false; all_q = false; some_p = false} in
 	let pre_i = fun i b -> if b then ca.(i).pre_true else ca.(i).pre_false in
 	let allp_i = fun i -> ca.(i).all_p in 
@@ -103,15 +106,99 @@ let build_cacheU d fd f = let ca = Array.make d.len {pre_false=false; pre_true=f
 			| LabelS atoms ->
 				let pre = fun b -> atoms.(p) || ( b  && atoms.(q) ) in
 				{ pre_false = pre false; pre_true = pre true; all_q = atoms.(q); some_p = atoms.(p)}
-			
-				 
-				 
-	let m_op = d.a
-	let pre = fun b -> 
-	
-	
-        
+	done		
+*)
 
+let bool_to_int = fun b -> if b then 1 else 0
+
+let build_pre = fun d fd ->
+	let pre = (Array.make_matrix (d.len) 2 (-1)) in
+	let all_q = Array.make d.len false in
+	let some_p = Array.make d.len false in
+	let (p,q) = (fd.d.(0),fd.d.(1)) in
+	for k = 0 to d.len - 1
+	do 
+		let dak = d.a.(k) in
+		match dak.n with 
+			LabelC label -> 
+				let children = dak.d in 
+				all_q.(k)  <- Array.fold_left (fun x i -> x && all_q.(i) ) true  children;
+				some_p.(k) <- Array.fold_left (fun x i -> x || some_p.(i)) false children;
+				let mI = children in 
+				let pre_ = fun b -> match label with 
+					'+'   ->  pre.(mI.(0)).(pre.(mI.(1)).(b))
+					| '>' ->  pre.(mI.(0)).(b)
+					| _   ->  bool_to_int (((b==1) || some_p.(k)) && all_q.(k)) in
+				( pre.(k).(0) <- pre_ 0;
+				  pre.(k).(1) <- pre_ 1 )
+			| LabelS atoms ->
+				let pre_ = fun b -> bool_to_int (atoms.(p) || ( (b==1)  && atoms.(q) )) in
+				( pre.(k).(0) <- pre_ 0;
+				  pre.(k).(1) <- pre_ 1;
+				  all_q.(k) <- atoms.(q);
+				  some_p.(k) <- atoms.(p) )
+	done
+
+let ttt=2
+(*	
+let add_atom_Upq_ = fun d_out d_in t_true t_false fd p q i ->
+	let pre = build_pre d_in fd in
+	let rec t_rec k b =
+		let t = fun i b -> let t_cache = if b then t_true else t_false in 
+			if t_cache.(i) < 0 then 
+				t_cache.(i) <- t_rec k b;
+			t_cache.(i)
+		let dak = d.a.(k) in
+		match dak.n with
+			LabelC label ->
+                                let mI = dak.d in 
+                                match label with 
+                                        '+'   ->  t(mI.(0), pre(mI.(1), b) +: t(mI.(1),b) 
+                                        | '<' ->  pre_i ( child.(0), b )
+                                        | '>' ->  pre_i ( child.(0), b )
+                                        | _   ->  (b || some_p) && all_q in
+                                { pre_false = pre false; pre_true = pre true; all_q = all_q; some_p = some_p}
+                        | LabelS atoms ->
+                                let pre = fun b -> atoms.(p) || ( b  && atoms.(q) ) in
+                                { pre_false = pre false; pre_true = pre true; all_q = atoms.(q); some_p = atoms.(p)}
+		t 
+*)
+
+	
+(*	
+let t_cache f a_true a_false i b = 
+	IF 
+	if a.(i) < 0 then
+		a.(i) <- f i;
+	a.(i) 
+		 
+let rec tau_ d_out d_in t_true t_false fd p q it_true t_false 
+let add_atom_Upq d p q  
+let add_atom_Upq_ = fun d_out d_in t_true t_false fd p q i ->
+	let pre = fun i b -> if b then ca.(i).pre_true else ca.(i).pre_false in
+	let pre i b = 
+	let rec t_rec k b =
+		let t = fun i b -> let t_cache = if b then t_true else t_false in 
+			if t_cache.(i) < 0 then 
+				t_cache.(i) <- t_rec k b;
+			t_cache.(i)
+		let dai = d.a.(k) in
+		match dai.n with
+			LabelC label ->
+                                let child = dai.d in 
+                                match label with 
+					let i = child.(0) in 
+                                        '+'   ->  let j = child.(1) in t(i, +: j) 
+                                        | '>' ->  pre_i ( child.(0), b )
+                                        | _   ->  (b || some_p) && all_q in
+                                { pre_false = pre false; pre_true = pre true; all_q = all_q; some_p = some_p}
+                        | LabelS atoms ->
+                                let pre = fun b -> atoms.(p) || ( b  && atoms.(q) ) in
+                                { pre_false = pre false; pre_true = pre true; all_q = atoms.(q); some_p = atoms.(p)}
+		t 
+	
+*)
+  
 
 
 
@@ -128,7 +215,6 @@ let (!<) = fun e -> let e_node = e.mc.med.a.(e.m).n in
   if e_node.pMHI < 0 then e_node.pMHI <- append {n=new_melement '<'; d=[|e.m|]} e.mc.med;
   {m=e_node.pMHI; mc = e.mc};
 *)
-let _ = print_dag (fun xx -> xx) ( dag_from_tree {l="a"; c=[{l="b";c=[]}]} );
  
 
 
@@ -159,4 +245,5 @@ type me = { meId : int; meSpace : melement array }
 
 let new_melement = fun o c -> {eOp=o; eCh=c; eIMH = -1 ; eHMI = -1}
 
+ddlet _ = print_dag (fun xx -> xx) ( dag_from_tree {l="a"; c=[{l="b";c=[]}]} );
 *)
