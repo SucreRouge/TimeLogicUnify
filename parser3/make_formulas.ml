da4169736f747cad63e5c621202fca8d3a931d68
John McCabe-Dansted <gmatht@gmail.com>
2012-08-13 20:13:25 +0800

    Sync: THINK-i7

---
+++ parser3/make_formulas.ml
@@ -0,0 +1,22 @@
+#!/bin/env ocaml
+
+let printf = Printf.printf
+let s = string_of_int
+let sm x = (s (x-1))
+let sp x = (s (x+1))
+let p i = "p" ^ (s i)
+let neg x = "~" ^ x
+let (+:) x y = "(" ^ x ^ "+" ^ y ^ ")"
+let u x y  = "U(" ^ x ^ ", " ^ y ^ ")"
+let (~<) x  = "<" ^ x 
+let rec me i = (if i<=0 then "{p1}" else 
+        ~<((p i) +: (me (i-1))))
+        (*"<(p"^s(i)^"+"^(me (i-1))^")" *)
+let alpha i = u (p i)  (neg(p (i+1)))
+
+let rec formula i = (if i<=1 then alpha 1 else alpha i ^ "|" ^ (formula (i-1) ))
+
+;;
+
+let ii = 9 in
+printf "%s : %s\n" (formula ii) (me ii)
