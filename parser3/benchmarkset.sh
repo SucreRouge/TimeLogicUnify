28e24c228f4b32bded61e49b46928b33ad14be7c
john <john@john-ThinkPad-E420.(none)>
2012-12-05 00:47:27 +0800

    Add linux specific stuff to benchmarks

--- parser3/benchmarkset.sh
+++ parser3/benchmarkset.sh
@@ -2,6 +2,10 @@ do_one () {
 	ocaml make_random_formulas_$1.ml $2 $3 $4 | /usr/bin/time ./main 2> output/set_$1_$2_$3_$4.err > output/set_$1_$2_$3_$4.out
 
 }
+mkdir -p output 
+
+sudo cpufreq-set -g performance
+cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
 
 for i in `seq 16`
 do
@@ -9,7 +13,6 @@ do
 	do_one 6s $e $e 99
 done
 
-exit
 for i in 000 010 020 050  
 do
 	do_one 6s $i 10000 1	
@@ -20,8 +23,15 @@ do
 	do_one 6s $i"00" 10000 1	
 done
 
-exit
 for i in 10 100 1000 10000 100000 1000000
 do
 	do_one 6s 100 $i 0	
 done
+
+for i in `seq 30`
+do
+        ocaml make_formulas.ml $i | /usr/bin/time ./main 2> output/$i.err > output/$i.out
+done
+
+
+sudo cpufreq-set -g ondemand
--- parser3/quickreport.sh
+++ parser3/quickreport.sh
@@ -1,2 +1,3 @@
- grep size *99.out |/usr/bin/sort -n -k6
-  grep user *99.err | /usr/bin/sort -t_ -n -k3
+ grep size output/*99.out |/usr/bin/sort -n -k6 ; grep user output/*99.err | /usr/bin/sort -t_ -n -k3
+ grep size output/*6s_100_*.out |/usr/bin/sort -n -k6 ; grep user output/*6s_100_*.err | /usr/bin/sort -t_ -n -k3
+
