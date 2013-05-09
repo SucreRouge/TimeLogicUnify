abc250b3c4701a0a90eb705e77e4be0521a1d1e3
John McCabe-Dansted <gmatht@gmail.com>
2013-05-09 20:23:18 +0800

    Sync: THINK-i7

--- parser3/benchpath.sh
+++ parser3/benchpath.sh
@@ -1,10 +1,13 @@
+formulas=mark_formulas.txt
+
+grep 'eg\[' var/mark/formulas/Examples.java  | grep '"' | sed 's/";//' | sed 's/.*"//' > $formulas 
 
 
-if [ 1 -gt "0$1"]
+if [ 1 -gt "0$1" ]
 then
-(echo '<'; cat  ../../4mlsolver/formulas.txt) | UNIFY_OFFLINE=y UNIFY_SOLVERS="BPATH BCTLNEW BCTLHUE BPATHf BPATHUE BPATHUEf" make runu
-#(echo '<'; cat  ../../4mlsolver/formulas.txt) | UNIFY_OFFLINE=y UNIFY_SOLVERS="BCTLNEW" make runu
-true && (echo '<'; cat  ../../4mlsolver/formulas.txt | sed 's/p/Ap/g
+(echo '<'; cat  $formulas) | UNIFY_OFFLINE=y UNIFY_SOLVERS="BPATH BCTLNEW BCTLHUE BPATHf BPATHUE BPATHUEf" make runu
+#(echo '<'; cat  $formulas) | UNIFY_OFFLINE=y UNIFY_SOLVERS="BCTLNEW" make runu
+true && (echo '<'; cat  $formulas | sed 's/p/Ap/g
 s/q/Aq/g
 s/r/Ar/g' ) | UNIFY_OFFLINE=y UNIFY_SOLVERS="BPATH BPATHUE" make runu
 fi
@@ -18,7 +21,7 @@ echo -n '' > var/out/benchmarkhue.tex
 	  echo "B-($L)"
   done
   cat
-) < ../../4mlsolver/formulas.txt | make runu
+) < $formulas | make runu
 
 (
 LOGICS='\mathcal{N} \mathcal{N}^{AE} \mathcal{N}^A \mathcal{L}'
