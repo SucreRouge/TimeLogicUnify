8b8c0a505d3378ba095253ea1f3f70a475a36d47
John McCabe-Dansted <gmatht@gmail.com>
2015-05-17 18:10:54 +0800

    Only require hue unique if all agents have STRONG strategies

---
+++ ATL/benchmark_MV.sh
@@ -0,0 +1,12 @@
+timeout 1600 ./do.pl "(Xp&-{1}Xp)" 99
+timeout 1600 ./do.pl "(Xp&-{2}Xp)" 
+timeout 1600 ./do.pl "(Xp&-{1}Xp)&{2}p" 
+(
+timeout 1600 ./do.pl "-w&{}G({1}X-w)&{2}Fw" 2
+timeout 1600 ./do.pl "-w&{}G({1}X-w)&{2}Xw" 2
+timeout 1600 ./do.pl "-w&{}G({1}X-w)&{2}XXw" 2
+timeout 1600 ./do.pl '{12}Xp&-{1}{2}Xp'
+timeout 1600 ./do.pl "-w&{}G({1}X-w)&{2}Fw"
+(BATL_USE_WEAK=N /usr/bin/time ./atl '{1}(XXp|XXq)&{}X({2}-Xp&{2}Xp)')
+) #2> sat2.err #> sat.out 
+ 
