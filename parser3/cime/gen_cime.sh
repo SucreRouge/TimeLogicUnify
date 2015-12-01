de9ebb70bcfe76cf3f22767adc9abdbbe5e62aa7
John McCabe-Dansted <gmatht@gmail.com>
2015-12-01 16:34:46 +0800

    Sync: john-VirtualBox

---
+++ parser3/cime/gen_cime.sh
@@ -0,0 +1,11 @@
+#< ../sample_rewrite_rules.txt sed 's/[+]/|/g
+grep -v '>' < ../sample_rewrite_rules.txt | sed '
+s/^[(]//g
+s/[)]$//g
+s/[*]/&/g
+s/\(.\)/\1 /g
+s/-/~/g
+s/>/ I /g
+s/=/ -> /g
+s/ $/;/g
+'
