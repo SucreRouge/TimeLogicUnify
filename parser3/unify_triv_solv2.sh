e6c78303af8bb290b5d1d007113b28db9e2afc66
John McCabe-Dansted <gmatht@gmail.com>
2015-12-30 17:47:03 +0800

    sync:

--- parser3/unify_triv_solv2.sh
+++ parser3/unify_triv_solv2.sh
@@ -11,7 +11,7 @@ do
 	# So just rerun unify multiple time and hope we get a bit further each time, manually cleaning up each time.
 	#for type in simp 
         echo "$solver" | tr "\n" "\t"
-	for type in orig simp 
+	for type in orig simp both
         #for type in orig
 do
 	   if [ $solver = null ] 
@@ -27,9 +27,9 @@ done | tee results/triv.out
 (
 cat <<EOF
 %created by 'make result' and 'unify_triv_solv2.sh'
-\begin{tabular}{|l|r|r|}
+\begin{tabular}{|l|r|r|r|}
 \hline
-Solver & Original & Simplified \\\\
+Solver & Original & Simplified & CiME+ \\\\
 \hline
 EOF
 sort -k3 < results/triv.out | ./csv2tex.pl  #> results/unify_len.out 
@@ -42,5 +42,5 @@ cp results/unify_triv$run.tex ~/uni/PhD || true
 }
 
 main "" 3  1000
-main 2  60 100
+main 2  60 1000
 
