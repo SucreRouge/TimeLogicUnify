de94ff33f2a8cc28a32a44a7b5034f1b8d41a729
John McCabe-Dansted <gmatht@gmail.com>
2016-01-05 17:47:02 +0800

    sync:

---
+++ parser3/cime/make_unify_trs_table2.tex.sh
@@ -0,0 +1,1 @@
+< confluent.trs tr -d ' ' | sed 's/(\([01abcpqr]\))/\1/g' | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- | sed -f trs2tex.sed | perl ../scripts/lines2matrix.pl > ~/uni/PhD/unify_trs_table2.tex
