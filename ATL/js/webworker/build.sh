4632ed8048ab232fb0a64f8f18cfcc6385ebd6f4
John McCabe-Dansted <gmatht@gmail.com>
2016-05-18 10:39:26 +0800

    Add trivial js_of_ocaml webworker example

---
+++ ATL/js/webworker/build.sh
@@ -0,0 +1,5 @@
+ocamlbuild -use-menhir -menhir "menhir --external-tokens Lexer" \
+  -pp "camlp4o -I /opt/local/lib/ocaml/site-lib js_of_ocaml/pa_js.cmo" \
+  -cflags -I,+js_of_ocaml,-I,+site-lib/js_of_ocaml -libs js_of_ocaml \
+  -lflags -I,+js_of_ocaml,-I,+site-lib/js_of_ocaml JsClient.byte
+js_of_ocaml JsClient.byte
