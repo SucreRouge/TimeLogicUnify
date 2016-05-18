04d33d9e89a267c23aa5e0f670b09738f0adfa08
John McCabe-Dansted <gmatht@gmail.com>
2016-05-18 17:36:44 +0800

    Cite source

--- ATL/js/webworker/build.sh
+++ ATL/js/webworker/build.sh
@@ -1,3 +1,5 @@
+# Cut down from:
+# http://toss.sourceforge.net/ocaml.html
 ocamlbuild -use-menhir -menhir "menhir --external-tokens Lexer" \
   -pp "camlp4o -I /opt/local/lib/ocaml/site-lib js_of_ocaml/pa_js.cmo" \
   -cflags -I,+js_of_ocaml,-I,+site-lib/js_of_ocaml -libs js_of_ocaml \
