872dc8ca8664f0f22fd20d62c9a33bc4f2574df8
John McCabe-Dansted <gmatht@gmail.com>
2016-05-26 16:09:07 +0800

    Now supports print_string

--- ATL/js/webworker/JsClient.ml
+++ ATL/js/webworker/JsClient.ml
@@ -3,40 +3,20 @@ let js_object = Js.Unsafe.variable "Object"
 let js_handler = jsnew js_object ()
 let postMessage = Js.Unsafe.variable "postMessage"
 
-let log s = ignore (Js.Unsafe.call postMessage (Js.Unsafe.variable "self")
-		      [|Js.Unsafe.inject (Js.string s)|])
-
-let onmessage event =
-  let fname = event##data##fname in
-  let args = event##data##args in
-  let handle = Js.Unsafe.get js_handler fname in
-  let result = Js.Unsafe.fun_call handle (Js.to_array args) in
-  let response = jsnew js_object () in
-  Js.Unsafe.set response (Js.string "fname") fname;
-  Js.Unsafe.set response (Js.string "result") result;
-  Js.Unsafe.call postMessage (Js.Unsafe.variable "self") [|Js.Unsafe.inject response|]
-
-let _ = Js.Unsafe.set (Js.Unsafe.variable "self") (Js.string "onmessage") onmessage
-(*
+let print_string s = Js.Unsafe.call postMessage (Js.Unsafe.variable "self") [|Js.Unsafe.inject (Js.string s)|]
+let print_endline s = (print_string (s^"\n"));;
+let print_newline () = (print_string "\n");;
+let caml_ml_output_char c = ();;
+let print_char c = ();;
+let printf fmt = 
+	let output_buffer = Buffer.create 1000 in
+	Printf.bprintf output_buffer fmt;;
 let url_argument x = List.assoc x Url.Current.arguments
-let url_argument x = let List.assoc x Url.Current.arguments
-let url_argc = 
-*)
-let url_argument x = List.assoc x Url.Current.arguments
-let url_argument x = List.assoc x [("arg1","XYA")]
+let url_argument x = List.assoc x [("arg1","XYA")];;
 
 let rec arg_str l = match l with 
 	(a,b)::tl -> "["^a^","^b^"]"^(arg_str tl)
 	| _ -> ""
 
-(* The NNF conversion and registration in JS. *)
-let js_nnf s = 
-  log ("computing nnf of " ^ (Js.to_string s));
-  Js.string (arg_str Url.Current.arguments)
-
-(*
-  Js.string ("NNF:" ^ (Js.to_string s) ^ (string_of_int (List.length Url.Current.arguments )) ^ (
-	let (a,b) = List.tl Url.Current.arguments in (":"^a^":"^b^":")  ) ^ "..." ^ (try url_argument "XYA" with _ -> "_") )
-*)
-let _ = Js.Unsafe.set js_handler (Js.string "nnf") (Js.wrap_callback js_nnf)
+let _ = print_endline "Goodbye"
    
