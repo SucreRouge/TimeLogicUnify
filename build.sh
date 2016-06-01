4bf811ab46d63df131d87851451435e6aab51a2d
John McCabe-Dansted <gmatht@gmail.com>
2016-06-01 09:14:22 +0800

    Add some new apt-get requirements.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 hash make ocamlopt rlwrap javac js_of_ocaml||
-	apt-get install make ocaml-nox rlwrap openjdk-7-jdk js-of-ocaml
+	apt-get install make ocaml-nox rlwrap openjdk-7-jdk js-of-ocaml libfindlib-ocaml libfindlib-ocaml-dev git-core
 
 cd parser3
 ./install_tableaux.sh
