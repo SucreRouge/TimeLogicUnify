25aa1aab3958d69c9c65d83ded48a408177c36c4
John McCabe-Dansted <gmatht@gmail.com>
2016-08-19 12:48:38 +0800

    Fix parsing of ANU ctl benchmark inputs

--- parser3/unify_benchmarks/anu_benchmarks.sh
+++ parser3/unify_benchmarks/anu_benchmarks.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
 cd unify_benchmarks || true
-(cd .. && make unify)
+export UNIFY_CPUS=1
+export UNIFY_TIMEOUT=60
+cd .. && make unify
 [ -e ctlcomparison ] || (
 	[ -e CTLComparisonBenchmarksNew.tgz ] || wget http://rsise.anu.edu.au/~rpg/CTLComparisonBenchmarks/CTLComparisonBenchmarksNew.tgz
 	tar -zxf CTLComparisonBenchmarksNew.tgz
 )
-(
-cd ctlcomparison/benchmarks
-echo '<'
-grep -v 'p25\|px\|q1' `find | grep ctl` | perl -pe 's/.*://;s/p([0-9]*)/chr(ord(a)+$1)/eg;s/ => / > /g;'
-) | (
-	cd ..
-	#UNIFY_CPUS=1 UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW BCTLHUE" ./unify
-	#UNIFY_CPUS=1 UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW mlsolve" ./unify
-	UNIFY_TIMEOUT=60 UNIFY_SOLVERS="mlsolver_simple_fair" nice ./unify
-) | tee ../results/anu_benchmark1.log 
+unify_benchmarks/convert_anu.sh|grep -v ERROR|
+	UNIFY_SOLVERS="mlsolver_simple_fair" nice ./unify|
+	tee results/anu_benchmark1.log
 
-(
-cd ctlcomparison/benchmarks
-echo '<'
-grep -v 'p25\|px\|q1' `find | grep ctl` | perl -pe 's/.*://;s/p([0-9]*)/chr(ord(a)+$1)/eg;s/ => / > /;s/^/AFGa>(/;s/$/)/;'
-) | (
-	cd ..
-	#UNIFY_CPUS=1 UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW BCTLHUE" ./unify
-	#UNIFY_CPUS=1 UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW mlsolve" ./unify
-	UNIFY_TIMEOUT=60 UNIFY_SOLVERS="BSHADES BCTLNEW mlsolver" nice ./unify
-) | tee ../result/anu_benchmark2.log 
+unify_benchmarks/convert_anu.sh|grep -v ERROR|perl -pe 's/^/AFGa>(/;s/$/)/'|
+	UNIFY_SOLVERS="BSHADES BCTLNEW mlsolver" nice ./unify|
+	tee results/anu_benchmark2.log
 
-(
-cd ctlcomparison/benchmarks
-echo '<'
-grep -v 'p25\|px\|q1' `find | grep ctl` | perl -pe 's/.*://;s/p([0-9]*)/chr(ord(a)+$1)/eg;s/ => / > /g;'
-) | (
-	cd ..
-	#UNIFY_CPUS=1 UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW BCTLHUE" ./unify
-	#UNIFY_CPUS=1 UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW mlsolve" ./unify
-	UNIFY_TIMEOUT=60 UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW mlsolver mlsolver_simple_fair" nice ./unify
-) | tee ../results/anu_benchmark3.log 
+unify_benchmarks/convert_anu.sh|grep -v ERROR|
+	 UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW mlsolver mlsolver_simple_fair" nice ./unify|
+	 tee results/anu_benchmark3.log
---
+++ parser3/unify_benchmarks/convert_anu.sh
@@ -0,0 +1,10 @@
+cd ctlcomparison/benchmarks
+echo '<'
+#grep -v 'p25\|px\|q1' `find | grep ctl` | perl -pe 's/.*://; # remove find garbage
+find | grep '[.]ctl$' | while read f; do cat $f; echo; done |
+perl -pe 's/.*://; # remove find garbage
+$i=0;%h=();s/\b([[:lower:]0-9]+)\b/if(not defined $h{$1}){$h{$1}=$i++};chr(ord(a)+$h{$1})/eg; #rename atoms to single letters
+if($i>26){s/.*/ERROR:TOO MANY ATOMS/}; #But we cant have more than 26 ASCII letters
+s/False/0/g;s/True/1/g; 
+s/ => / > /g;'
+#s/p([0-9]*)/chr(ord(a)+$1)/eg;
