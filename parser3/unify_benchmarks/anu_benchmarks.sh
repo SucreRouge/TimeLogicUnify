679545b5d97532397c4462da95ce888ff5de55c2
John McCabe-Dansted <gmatht@gmail.com>
2016-09-03 13:33:23 +0800

    Changes from benchmark machine

--- parser3/unify_benchmarks/anu_benchmarks.sh
+++ parser3/unify_benchmarks/anu_benchmarks.sh
@@ -11,10 +11,11 @@ unify_benchmarks/convert_anu.sh|grep -v ERROR|
 	UNIFY_SOLVERS="mlsolver_simple_fair" nice ./unify|
 	tee results/anu_benchmark1.log
 
-unify_benchmarks/convert_anu.sh|grep -v ERROR|perl -pe 's/^/AFGa>(/;s/$/)/'|
-	UNIFY_SOLVERS="BSHADES BCTLNEW mlsolver" nice ./unify|
+	unify_benchmarks/convert_anu.sh|grep -v ERROR|perl -pe 'if (/^[^<]/){s/^/AFGa>(/;s/$/)/}'|
+	UNIFY_DO_POS="N" UNIFY_SOLVERS="BSHADES BCTLNEW BCTLHUE mlsolver" nice ./unify|
 	tee results/anu_benchmark2.log
 
 unify_benchmarks/convert_anu.sh|grep -v ERROR|
-	 UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW mlsolver mlsolver_simple_fair" nice ./unify|
+	UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW BCTLHUE mlsolver mlsolver_simple_fair" nice ./unify|
+	 #UNIFY_SOLVERS="ctl-rp anu-tr anu-bdd BSHADES BCTLNEW mlsolver mlsolver_simple_fair" nice ./unify|
 	 tee results/anu_benchmark3.log
