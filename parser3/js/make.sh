728c315ffb5f91d11cff116adc99bace0356b0d6
John McCabe-Dansted <gmatht@gmail.com>
2016-09-23 16:51:46 +0800

    Fix missing "-libs str" in mechecker.js

--- parser3/js/make.sh
+++ parser3/js/make.sh
@@ -3,4 +3,4 @@
 	ln -s ../main.ml mechecker.ml
 )
 grep -v html < ../mechecker_header.html > mechecker.text.html
-SAMPLE_INPUT="U(p,-q) : [{p};{p,q};{}]+<{p}" ../../ATL/js/webworker/ml2js.sh mechecker "U(p,-q) : [{p};{p,q};{}]+<{p}"
+SAMPLE_INPUT="U(p,-q) : [{p};{p,q};{}]+<{p}" ../../ATL/js/webworker/ml2js.sh mechecker "U(p,-q) : [{p};{p,q};{}]+<{p}" "-libs str"
