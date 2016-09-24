ae4082bcf1da34947b5ff5678b4b5cd0ad70260d
John McCabe-Dansted <gmatht@gmail.com>
2016-09-24 10:54:28 +0800

    Add missing .

--- parser3/js/make.sh
+++ parser3/js/make.sh
@@ -1,5 +1,5 @@
 [ -e mechecker.ml ] || ( 
-	ln -s ../mainlib.ml ../me_lexer.mll ../me_parser.mly ../phi_lexer.mll ../phi_parser.mly 
+	ln -s ../mainlib.ml ../me_lexer.mll ../me_parser.mly ../phi_lexer.mll ../phi_parser.mly .
 	ln -s ../main.ml mechecker.ml
 )
 grep -v html < ../mechecker_header.html > mechecker.text.html
