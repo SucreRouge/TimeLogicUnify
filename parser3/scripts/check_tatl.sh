d72ebf33d25cd7a0103a7b0180810cf94730760b
John McCabe-Dansted <gmatht@gmail.com>
2016-10-28 14:25:34 +0800

    sync: john-Virtual1604 Intel(R) Celeron(R) CPU  N3050  @ 1.60GHz

--- parser3/scripts/check_tatl.sh
+++ parser3/scripts/check_tatl.sh
@@ -17,6 +17,6 @@ do
 	echo
 	echo "--- $d ---"
 	#grep "$d" -B7 fuzztest.out fuzztest1.out | egrep 'Nega|fied.to' | sed s/.*:\ // | sed -f scripts/2tatl.sed
-	cat fuzztest1.out | egrep 'RULE|Simplified.to|Negation' | grep "$d" -B1 | egrep 'Nega|fied.to' | sed s/.*:\ // | sed -f scripts/2tatl.sed | sed 's/^/<<1>>(/' | sed 's/$/)/'
+	cat fuzztest2.out | egrep 'RULE|Simplified.to|Negation' | grep "$d" -B1 | egrep 'Nega|fied.to' | sed s/.*:\ // | sed -f scripts/2tatl.sed | sed 's/^/<<1>>(/' | sed 's/$/)/'
 done
 
--- parser3/scripts/fuzztest.sh
+++ parser3/scripts/fuzztest.sh
@@ -1,3 +1,3 @@
-ocaml make_random_ctls_formulas.ml 15 1 10000 99999 | 
-UNIFY_SOLVERS="mlsolver tatl" nice -16 make runu | tee fuzztest1.out
+ocaml make_random_ctls_formulas.ml 15 2 10000 999999 | 
+UNIFY_SOLVERS="mlsolver tatl" nice -16 make runu | tee fuzztest2.out
 	
